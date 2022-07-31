3fb1326deb90154e5c3c5c018eda8f20f8be82ef
Tiago César Oliveira <tiago.oliveira@messagebird.com>
2022-07-31 16:15:27 +0200

    Monorepo structure for importer and api microservices; profobuf schema configuration for service-to-service communication

---
+++ internal/schema/schema.go
@@ -0,0 +1,11 @@
+package schema
+
+type Geolocation struct {
+	IpAddress    string  `json:"ip_address"`
+	CountryCode  string  `json:"country_code"`
+	Country      string  `json:"country"`
+	City         string  `json:"city"`
+	Latitude     float64 `json:"latitude"`
+	Longitude    float64 `json:"longitude"`
+	MysteryValue string  `json:"mystery_value"`
+}
